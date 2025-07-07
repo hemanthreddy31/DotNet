288c559eae3f574feff1c7c6b110fe3435552f94
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2025-07-07 14:17:08 +0530

    dotnet test projects

---
+++ 06Testing/UnitTesting/XUnitTests/XUnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace XUnitTests
+{
+    public sealed class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [InlineData(true)]
+        [Theory]
+        public void Test2(bool setbit)
+        {
+            return false;
+        }
+    }
+}
